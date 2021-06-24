40689ec88ced0b4eefe6f65b970320c311ea09e1
João Coutinho <jafonsocoutinho@protonmail.com>
2021-06-24 22:53:39 +0100

    Added README

---
+++ README.md
@@ -0,0 +1,5 @@
+# RSA Signatures
+
+Python implementation of the signature scheme described in [short and stateless signature from the RSA assumption](short_and_stateless_signatures.pdf).
+
+*NOTE: [Project.md](Project.md) was composed and can be properly rendered using [Obsidian](https://obsidian.md).*
--- test.py
+++ test.py
@@ -1,12 +1,18 @@
 from rsaSignatures import *
+import sys
 
 if __name__ == '__main__':
-    print("Generating key pair")
+    print("Generating key pair...", end="")
+    sys.stdout.flush()
+
     k = KeyPair(12)
+    print("DONE")
 
     with open("verse.txt", "r") as f:
         message = f.read()
 
+    print("Signing...", end="")
+    sys.stdout.flush()
     # Weak sign
     signature = weakSign(k.sk, message, k.constants)
     weak = weakVerify(k.pk, message, signature, k.constants)
@@ -14,6 +20,7 @@ if __name__ == '__main__':
     # Strong sign
     signature = strongSign(k.sk, message, k.constants)
     strong = strongVerify(k.pk, message, signature, k.constants)
+    print("DONE")
 
     print("Weak: ", weak)
     print("Strong: ", strong)
